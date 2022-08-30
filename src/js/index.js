cb501ff68e2e04f519c938ab4a165cd28f0451ca
MikeMoore55 <michaelwillemmoore@gmail.com>
2022-08-30 15:21:48 +0200

    added loading screen
    made loading screen so all content can be loaded

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,22 @@
 /* nav for mobile view */
+function onReady(callback) {
+  var intervalId = window.setInterval(function() {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 1000);
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+  setVisible('.page', true);
+  setVisible('#loading', false);
+});
+
 
 const nav = document.querySelector("#nav-bar-mobile")
 
